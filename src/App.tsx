37cdb0205ab18d5d5d2b02047911550f270e71db
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-01-15 11:27:11 -0500

    Remove BudgetContext and ExpensesContext providers

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { Paper } from "@mantine/core";
 import { MantineProvider } from "@mantine/core";
 import MainAppShell from "./components/MainAppShell";
 import { AvailableCategoriesContextProvider } from "./store/AvailableCategoriesContext";
-import { BudgetContextProvider } from "./store/BudgetContext";
 import { ExpenseCategoriesContextProvider } from "./store/ExpenseCategoriesContext";
-import { ExpensesContextProvider } from "./store/ExpensesContext";
 import { HistoryContextProvider } from "./store/HistoryContext";
 
 export default function App() {
@@ -20,15 +18,11 @@ export default function App() {
       <Paper>
         {/* Contexts */}
         <AvailableCategoriesContextProvider>
-          <BudgetContextProvider>
-            <ExpensesContextProvider>
               <HistoryContextProvider>
                 <ExpenseCategoriesContextProvider>
                   <MainAppShell />
                 </ExpenseCategoriesContextProvider>
               </HistoryContextProvider>
-            </ExpensesContextProvider>
-          </BudgetContextProvider>
         </AvailableCategoriesContextProvider>
       </Paper>
     </MantineProvider>
